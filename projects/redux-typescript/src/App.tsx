e18f49148450e4120f9180d2656a6a727be39fd2
turbofuture1 <alimbadmus239@gmail.com>
2022-01-08 19:03:24 +0000

    finished

--- projects/redux-typescript/src/App.tsx
+++ projects/redux-typescript/src/App.tsx
@@ -1,56 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state";
+import { State } from "./state/reducers";
 
 function App() {
+  const dispatch = useDispatch();
+  const { depositMoney, bankruptMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const amount = useSelector((state: State) => state.bank);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>{amount}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(500)}>Withdraw</button>
+      <button onClick={() => bankruptMoney()}>Bankrupt</button>
     </div>
   );
 }
---
+++ projects/redux-typescript/src/state/store.ts
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from "@reduxjs/toolkit";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+
+
+
+
+export const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(thunk)
+)
