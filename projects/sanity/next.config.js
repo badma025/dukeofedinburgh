0184dc88187c5c68887e33a7d709655fe1bef6c8
turbofuture1 <alimbadmus239@gmail.com>
2022-01-22 17:29:28 +0000

    added/updated

--- projects/sanity/next.config.js
+++ projects/sanity/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  },
+};
