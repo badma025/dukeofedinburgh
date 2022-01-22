ad808192528175f0bc04385c87cee17430b5e568
turbofuture1 <alimbadmus239@gmail.com>
2022-01-22 20:58:54 +0000

    updated on 22/01/2022

--- projects/sanity/sanity.js
+++ projects/sanity/sanity.js
@@ -4,15 +4,10 @@ import {
   createClient,
 } from "next-sanity";
 
-interface IConfig {
-  dataset: string;
-  projectId: string;
-  apiVersion: string;
-  useCdn: boolean;
-}
 
-export const config: IConfig = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
