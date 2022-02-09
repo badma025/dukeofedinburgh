fa062847ca3f80d1f207ce5a4be301fcc3e65a75
turbofuture1 <alimbadmus239@gmail.com>
2022-02-09 20:21:50 +0000

    updated on 09/02/2022

---
+++ projects/C++/tutorial/functions/overloaded-functions/index.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+void bakePizza(string bread, string sauce, string cheese, string topping)
+{
+    cout << "Here is your ";
+    cout << bread << " ";
+    cout << sauce << " ";
+    cout << cheese << " ";
+    cout << topping << " ";
+    cout << " pizza";
+}
+
+void bakePizza(string bread, string sauce, string cheese)
+{
+    cout << "Here is your ";
+    cout << bread << " ";
+    cout << sauce << " ";
+    cout << cheese << " ";
+    cout << " pizza";
+}
+void bakePizza(string bread, string sauce)
+{
+    cout << "Here is your ";
+    cout << bread << " ";
+    cout << sauce << " ";
+    cout << " pizza";
+}
+void bakePizza(string bread)
+{
+    cout << "Here is your ";
+    cout << bread << " ";
+    cout << " pizza";
+}
+
+int main()
+{
+    string bread;
+    string sauce;
+    string cheese;
+    string topping;
+}
