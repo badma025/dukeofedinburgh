57ad035272e57216384763bbd3d2a02ce30997d2
turbofuture1 <alimbadmus239@gmail.com>
2022-09-19 22:29:01 +0100

    updated

--- projects/sanity/sanity/schemas/comment.js
+++ projects/sanity/sanity/schemas/comment.js
@@ -13,7 +13,6 @@ export default {
       type: "boolean",
       description: "Comments won't show on the site without approval",
     },
-    { name: "email", type: "string" },
     { name: "comment", type: "text" },
     { name: "post", type: "reference", to: [{ type: "post" }] },
   ],
